fbcb7372b981d6c196d2cfa9407dc3f7837bc4d0
claudejones <claudejones43@yahoo.com>
2026-09-17 13:39:42 -0500

    Phase 7C add production bootstrap boundary

---
+++ src/js/bootstrap/production-bootstrap.js
@@ -0,0 +1,18 @@
+(() => {
+  "use strict";
+
+  const BUILD = Object.freeze({
+    phase: "7C",
+    kind: "production",
+    modes: Object.freeze(["game"])
+  });
+
+  window.CC_APP = Object.freeze({
+    build: BUILD,
+    mode: "game",
+    isDevelopment: false
+  });
+
+  document.documentElement.dataset.appBuild = "production";
+  document.documentElement.dataset.appMode = "game";
+})();
